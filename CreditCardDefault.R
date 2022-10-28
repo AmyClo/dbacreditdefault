99d7464fd16d197611ed2abcb577f755896544ca
jennyohwait <116596447+jennyohwait@users.noreply.github.com>
2022-10-28 11:16:02 +0200

    DataPrep+knn+c50+cart+simplifiedRF(Upsample)

--- CreditCardDefault.R
+++ CreditCardDefault.R
@@ -40,13 +40,18 @@ summary(upsample)
 upsample$SEX <- as.character(upsample$SEX)
 upsample$EDUCATION <- as.character(upsample$EDUCATION)
 upsample$MARRIAGE <- as.character(upsample$MARRIAGE)
-dummydata_up <- dummyVars("~ .", data = upsample[,2:4], fullRank = T)
+dummydata_up <- dummyVars("~ .", data = upsample[,2:4])
 trsf_up <- data.frame(predict(dummydata_up, newdata = upsample[,2:4]))
+trsf_up$SEX2<-NULL
+trsf_up$EDUCATION4<-NULL
+trsf_up$MARRIAGE3<-NULL
+
 #rearrange df
 new_up <- upsample[,5:24]
 new_up <- new_up[c(20,1:19)]
 new_up[,21] <- upsample[,1]
 new_up[,22:27] <- trsf_up
+
 #standarization
 library(caret)
 preproc_up <- preProcess(new_up[,2:27], method = c ("range"))
@@ -54,33 +59,99 @@ trsf_preproc_up <- data.frame(predict(preproc_up, newdata = new_up[,2:27]))
 up_preproc <- trsf_preproc_up
 up_preproc[,27] <- new_up[,1]
 
-names(up_preproc)[names(up_preproc) == "V27"] <- "Default_NXTM"
+names(up_preproc)[names(up_preproc) == "V27"] <- "Default"
 names(up_preproc)[names(up_preproc) == "V21"] <- "LIMIT_BAL"
-names(up_preproc)[names(up_preproc) == "SEX2"] <- "Female"
-
+names(up_preproc)[names(up_preproc) == "SEX1"] <- "Male"
+names(up_preproc)[names(up_preproc) == "EDUCATION1"] <- "GraduateSchool"
+names(up_preproc)[names(up_preproc) == "EDUCATION2"] <- "University"
+names(up_preproc)[names(up_preproc) == "EDUCATION3"] <- "HighSchool"
+names(up_preproc)[names(up_preproc) == "MARRIAGE1"] <- "Married"
+names(up_preproc)[names(up_preproc) == "MARRIAGE2"] <- "Single"
 
 #partition for upsample data
-testidx_up <- createDataPartition(up_preproc$Default_NXTM, p = 0.2, list = FALSE)
+testidx_up <- createDataPartition(up_preproc$Default, p = 0.2, list = FALSE)
 testidx_up
 data_train_up <- up_preproc[-testidx_up,]
 data_test_up <- up_preproc[testidx_up,]
 
-summary(data_test_up$Default_NXTM)
-summary(data_train_up$Default_NXTM)
+summary(data_test_up$Default)
+summary(data_train_up$Default)
 
-#creat report
+#create report
 report <- data.frame(Model=character(), Acc.Train=numeric(), Acc.Test=numeric())
 
-#knn
-library(class)
-ks <- 3:25
-for (k in ks[seq(1, length(ks), 2)])
-{
-  knnmodel_up <- knn(train=data_train_up[,-27], test=data_test_up[,-27], cl=data_train_up$Default_NXTM, k=k)
-  num.correct.labels <- sum(knnmodel_up == data_test_up$Default_NXTM)
-  accuracy <- num.correct.labels / length(data_test_up$Default_NXTM)
-  cat(k, ", ", accuracy, "\n")
-}
+TControl <- trainControl(method="cv", number=10)
 
-summary(knnmodel_up)
+#knn
+knnGrid_up <- expand.grid(k = c(seq(3, 25, 2)))
+knnmodel_up <- train(Default~., data=data_train_up, method="knn", tuneGrid=knnGrid_up, trControl=TControl)
+knnmodel_up
+prediction.train <- predict(knnmodel_up, data_train_up[,-27], type="raw")
+prediction.test <- predict(knnmodel_up, data_test_up[,-27],type="raw")
+acctr <- confusionMatrix(prediction.train, data_train_up[,27])
+acctr$table
+acctr$overall['Accuracy']
+accte <- confusionMatrix(prediction.test, data_test_up[,27])
+accte$table
+accte$overall['Accuracy']
+report <- rbind(report, data.frame(Model="k-NN", Acc.Train=acctr$overall['Accuracy'], Acc.Test=accte$overall['Accuracy']))
+report
+
+#C5.0
+set.seed(123)
+c50Grid <- expand.grid(trials = c(1:5),
+                       model = c("tree", "rules"),
+                       winnow = FALSE)
+c5model <- train(Default ~., data=data_train_up, method="C5.0", tuneGrid=c50Grid, trControl=TControl)
+c5model 
+prediction.trainC5 <- predict(c5model, data_train_up[,-27], type="raw")
+prediction.testC5 <- predict(c5model, data_test_up[,-27], type="raw")
+acctrC5 <- confusionMatrix(prediction.trainC5, data_train_up[,27])
+acctrC5$table
+acctrC5$overall['Accuracy']
+accteC5 <- confusionMatrix(prediction.testC5, data_test_up[,27])
+accteC5$table
+accteC5$overall['Accuracy']
+report <- rbind(report, data.frame(Model="C5.0", Acc.Train=acctrC5$overall['Accuracy'], Acc.Test=accteC5$overall['Accuracy']))
+report
+
+#CART
+set.seed(123)
+cartmodel <- train(Default ~., data=data_train_up, method="rpart", trControl=TControl)
+cartmodel 
+prediction.trainCA <- predict(cartmodel, data_train_up[,-27], type="raw")
+prediction.testCA <- predict(cartmodel, data_test_up[,-27], type="raw")
+acctrCA <- confusionMatrix(prediction.trainCA, data_train_up[,27])
+acctrCA$table
+acctrCA$overall['Accuracy']
+accteCA <- confusionMatrix(prediction.testCA, data_test_up[,27])
+accteCA$table
+accteCA$overall['Accuracy']
+report <- rbind(report, data.frame(Model="CART", Acc.Train=acctrCA$overall['Accuracy'], Acc.Test=accteCA$overall['Accuracy']))
+report
+
+
+#Decision Tree
+
+
+
+#Random Forest
+library(randomForest)
+RFmodel_up <- randomForest(Default ~ ., data = data_train_up)
+RF_prediction <- predict(RFmodel_up, data_test_up)
+y1 <- data_test_up$Default 
+y_pred_v1 <- RF_prediction
+
+precision <- posPredValue(y_pred_v1, y1)
+recall <- sensitivity(y_pred_v1, y1)
+F1 <- (2*precision*recall) / (precision+recall)
+
+precision #0.9669187 accuracy
+recall #0.8895652 proportion of positive results out of samples
+F1 #0.9266304 concise the previous metrics and the harmonic average
+
+importance(RFmodel_up,)
+
+
+
 
